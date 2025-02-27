d90087b453cf756d62437498e81d63164702f39c
Hashem Erainat <h.erainat1@student.aaup.edu>
2025-02-27 22:49:53 +0200

    test_newbranch

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      title: 'E-commerc App',
+      title: 'E-commerc FLutter App',
       home: CustomBottomNavbar(),
     );
   }
